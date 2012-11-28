29e9160ddbb2e1e526b2a72f7226acd677859e47
lucybastin (Lucy Bastin) <l.bastin@aston.ac.uk>
2012-11-28 08:29:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xmlschemareader@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ extras/test_code.R
@@ -0,0 +1,91 @@
+# install.packages("inlinedocs",repos="http://r-forge.r-project.org")
+# library(inlinedocs)
+# package.skeleton.dx("C:/subversion/Rforge_XML_Schema_reader/xmlSchemaReader")
+
+library ("xmlschemareader")
+
+o <- getAttributesAndElementsForElementName("CentredMoment", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("ConfusionMatrix", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("CredibleInterval", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("InterquartileRange", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Median", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Percentile", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Quartile", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("StandardDeviation", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("CoefficientOfVariation", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Correlation", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Decile", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Kurtosis", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Mode", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Probability", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Range", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("ConfidenceInterval", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("CovarianceMatrix", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("DiscreteProbability", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Mean", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Moment", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Quantile", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Skewness", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Variance", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("BernoulliDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("CauchyDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("ExponentialDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("GeometricDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("LaplaceDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("MixtureModel", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("MultivariateStudentTDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("NormalInverseGammaDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("StudentTDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("WishartDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("BetaDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("ChiSquareDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("FDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("HypergeometricDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("LogisticDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("MultinomialDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("NegativeBinomialDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("ParetoDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("UniformDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("BinomialDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("DirichletDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("GammaDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("InverseGammaDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("LogNormalDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("MultivariateNormalDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("NormalDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("PoissonDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("WeibullDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("RandomSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("Realisation", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("SystematicSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("UnknownSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+
+
+# Just the problematic ones...
+o <- getAttributesAndElementsForElementName("MixtureModel", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("MultivariateNormalDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("NormalDistribution", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("RandomSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("SystematicSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+o <- getAttributesAndElementsForElementName("UnknownSample", "C:/subversion/Rforge_XML_Schema_reader/extras/uncertml.xsd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
